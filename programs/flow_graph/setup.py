7d4ee282582526cb8b3a8acd6c8f27016b90eab7
Veiasai <o206@qq.com>
2023-08-13 03:32:36 +0000

    init

---
+++ programs/flow_graph/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="Flow Graph",
+    version="1.0",
+    description="",
+    author="Veiasai",
+    author_email="o206@qq.com",
+    url="",
+    packages=["flow_graph"],
+)
